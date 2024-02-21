f60f3b6a2f9e56f407b13e83dc7abe24d895b4df
Alexandre Faure <alexandre.faure@student-cs.fr>
2024-02-21 18:43:55 +0100

    Handle breadcrumb

--- posts/static/posts/js/report.js
+++ posts/static/posts/js/report.js
@@ -13,13 +13,16 @@ $('.report-form').submit(function(e){
             'post_id':post_id,
         },
         success: function(response) {
-            console.log('Post reported', response['report_added'])
             const button_str_id = '#report_button_' + post_id
             if(response['report_added']) {
                 $(button_str_id).addClass('red')
             } else {
                 $(button_str_id).removeClass('red')
             }
+            // Ajout du toast
+            let toast = response['toast_html'];
+            console.log('toast', toast)
+            $('#toasts-container').append(toast);
         },
         error: function(response) {
             console.log('error', response)
